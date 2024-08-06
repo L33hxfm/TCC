1cee6c2f76c5583c28ddee57564ecd63ddb39bf6
L33hxfm <151097168+L33hxfm@users.noreply.github.com>
2024-08-06 12:20:03 -0300

    Update script_email.js

--- JS/script_email.js
+++ JS/script_email.js
@@ -2,15 +2,7 @@ document.getElementById('recuperarSenhaBtn').addEventListener('click', () => {
     const emailSiga = document.getElementById('email_Siga').textContent;
   
     fetch('https://your-vercel-app-name.vercel.app/recuperar-senha', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ email: emailSiga }),
-    })
-    .then(response => response.json())
-    .then(data => {
-      alert(data.message);
+      method: 'POST'
     })
     .catch(error => {
       console.error('Erro:', error);
